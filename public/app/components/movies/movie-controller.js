d7738813acd62927fa531d47392afd42de71cb61
Jonah Engelund <jonahengelund@gmail.com>
2018-11-30 17:21:55 -0700

    jonah

--- public/app/components/movies/movie-controller.js
+++ public/app/components/movies/movie-controller.js
@@ -2,24 +2,43 @@ import MovieService from "./movie-service.js"
 
 let _mS = new MovieService()
 
-
-function getMovie() {
-
+function handleError(err) {
+  console.error("Sorry 0 results found")
 }
 
 function _drawMovie(movies) {
-  console.log(movies)
+  console.log(movies);
+  let template = ''
+  movies.forEach(movie => {
+    let imageUrl = "https://image.tmdb.org/t/p/w150_and_h150_bestv2" + movie.poster_path
+    template += `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-start contain" src="${imageUrl}" alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">${movie.overview}</p>
+    <a href="#" class="btn btn-primary">Add Review</a>
+    </div>
+    </div>
+    `
+  });
+  document.getElementById('movieCard').innerHTML = template
 }
 
 
 
 export default class MovieController {
   constructor() {
-    console.log('movie controller working')
 
   }
   searchMovie(event) {
     event.preventDefault()
-    _mS.getMovie(event.target.search.value, _drawMovie)
+    _mS.getMovies(event.target.search.value, _drawMovie)
   }
+  getMovies() {
+    _mS.getMovies(_drawMovie, handleError)
+  }
+  // getOurMovie(id, getMovies) {
+  //   _mS.getOurMovie(id, _drawMovie, getMovies)
+  // }
 }
--- public/app/components/movies/movie-service.js
+++ public/app/components/movies/movie-service.js
@@ -1,5 +1,4 @@
 import Movie from "./models/movie.js"
-
 // @ts-ignore
 let _movieAPI = axios.create({
   baseURL: 'https://api.themoviedb.org/3/search',
@@ -7,20 +6,36 @@ let _movieAPI = axios.create({
 })
 
 let _apiKey = "api_key=1acfaa233b759fc415e78d406552edcd&query="
-
 let _movies = []
+
+// @ts-ignore
+let _flixbucketdb = axios.create({
+  baseURL: 'mongodb://moviewatcher1:moviewatcher1@ds119164.mlab.com:19164/flixbucketdb',
+  timeout: 3000
+})
+
 
 export default class MovieService {
   contructor() {
     console.log('movie service working')
-
   }
-  getMovie(query, _drawMovie) {
+  getMovies(query, _drawMovie, handleError) {
     _movieAPI.get("/movie?" + _apiKey + query)
       .then(res => {
-        _drawMovie(res.data.results)
+        let movies = res.data.results
+        _drawMovie(movies)
+      })
+      .catch(err => {
+        console.log(err)
       })
   }
-
+  // getOurMovie(title, _drawMovie, getMovies) {
+  //   _flixbucketdb.get(title)
+  //     .then(res => {
+  //       _movies = res.data.results
+  //       _drawMovie()
+  //     })
+  //     .catch(this.getMovies())
+  // }
 
 }
